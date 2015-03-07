a0121c154ba34f70bcdcb24cbabef9f55bcc6218
rmook <richard.mook@gmail.com>
2015-03-07 15:20:15 -0500

    added slidetoggle function to beer and wells menus/nav links

--- js/script.js
+++ js/script.js
@@ -4,4 +4,28 @@
 // 	Clicking a menu item hides any 
 // 	currently shown menu and displays the new one
 
+var menubeer = $(".beer");
+var menuwells = $(".wells");
 
+$("#beer").click(function() {
+	menuwells.hide();
+	menubeer.slideToggle("slow")
+});
+
+$("#wells").click(function() {
+	menubeer.hide();
+	menuwells.slideToggle("slow")
+});
+
+
+// Problem:
+// 	When a menu item is clicked, nothing happens.
+// Solution:
+// 	Clicking a menu item displays the text of that menu item full-size.
+// 	
+
+// Problem:
+// 	When the device is shaken, nothing happens.
+// Solution:
+// 	If a menu item is displayed, shaking the 
+// 	phone will replace that item with "Thanks!"
