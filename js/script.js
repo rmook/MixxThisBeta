faba83fef19b7bf7eab7a65bc14d63c2d5241aa9
rmook <richard.mook@gmail.com>
2015-03-07 16:43:49 -0500

    added social links, adjusted JS accordingly

--- js/script.js
+++ js/script.js
@@ -8,22 +8,34 @@ var navColor = "#b8b6b1";
 var selectedColor = "#ffbd49";
 var menuBeer = $(".beer");
 var menuWells = $(".wells");
+var menuSocial = $(".social")
 
 $("#beer").click(function() {
 	menuWells.hide();
+	menuSocial.hide();
 	$("#wells").css("background-color",navColor);
+	$("#social").css("background-color", navColor);
 	$("#beer").css("background-color",selectedColor);
 	menuBeer.slideToggle("slow")
 });
 
 $("#wells").click(function() {
 	menuBeer.hide();
+	menuSocial.hide();
 	$("#beer").css("background-color",navColor);
+	$("#social").css("background-color", navColor);
 	$("#wells").css("background-color",selectedColor);
 	menuWells.slideToggle("slow")
 });
 
-
+$("#social").click(function() {
+	menuBeer.hide();
+	menuWells.hide();
+	$("#beer").css("background-color",navColor);
+	$("#wells").css("background-color",navColor);
+	$("#social").css("background-color",selectedColor);
+	menuSocial.slideToggle("slow")
+});
 // Problem:
 // 	When a menu item is clicked, nothing happens.
 // Solution:
